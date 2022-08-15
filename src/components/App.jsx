b382e16f648d5aed966d03af452d69773ceaeb74
Vitaliy Zhoglo <zhoglo.vitaliy@gmail.com>
2022-08-16 00:23:58 +0300

    part of task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,98 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { GlobalStyles } from './GlobalStyles';
+import { nanoid } from 'nanoid';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+    name: '',
+    number: '',
+  };
+
+  onChange = e => {
+    const { name } = e.target;
+    this.setState({
+      [name]: e.target.value,
+    });
+  };
+
+  reset = () => {
+    this.setState({
+      name: '',
+      number: '',
+    });
+  };
+
+  createContact = () => {
+    return {
+      id: nanoid(5),
+      name: this.state.name,
+      number: this.state.number,
+    };
+  };
+
+  onSubmit = evt => {
+    evt.preventDefault();
+    const contact = this.createContact();
+    this.setState(prevState => ({
+      contacts: [contact, ...prevState.contacts],
+    }));
+    this.reset();
+  };
+
+  // evt.target.elements.name.value;
+
+  render() {
+    return (
+      <>
+        <form onSubmit={this.onSubmit}>
+          <label>
+            Name
+            <input
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+              value={this.state.name}
+              onChange={this.onChange}
+            />
+          </label>
+          <label>
+            Number
+            <input
+              type="tel"
+              name="number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+              value={this.state.number}
+              onChange={this.onChange}
+            />
+          </label>
+          <button type="submit">Add contact</button>
+        </form>
+        <div>
+          <p>Contacts</p>
+          <input type="text" name="filter" value={this.state.filter} />
+          <ul>
+            {this.state.contacts.map(contact => {
+              return (
+                <li key={contact.id}>
+                  {contact.name} : {contact.number}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <GlobalStyles />
+      </>
+    );
+  }
+}
---
+++ src/theme.jsx
@@ -0,0 +1,46 @@
+export const theme = {
+  colors: {
+    black: '#000',
+    white: '#fff',
+    text: '#20b9ce',
+    total: '#057d8d',
+    background: '#fff',
+    primary: '#7547e0',
+    secondary: '#592ac7',
+    accent: '#9174fa',
+    muted: '#f6f6f6',
+    percent: '#023a7a',
+  },
+  space: [0, 2, 4, 8, 16, 32, 64, 128, 256],
+  fonts: {
+    body: 'Roboto, sans-serif',
+    heading: 'system-ui, sans-serif',
+    monospace: 'Menlo, monospace',
+  },
+  fontSizes: {
+    xs: '14px',
+    s: '16px',
+    m: '24px',
+    l: '32px',
+    xl: '64px',
+  },
+  fontWeights: {
+    normal: 400,
+    bold: 700,
+  },
+  lineHeights: {
+    body: 1.5,
+    heading: 1.125,
+  },
+  borders: {
+    none: 'none',
+    normal: '1px solid',
+  },
+  radii: {
+    none: '0',
+    sm: '4px',
+    md: '8px',
+    lg: '16px',
+    round: '50%',
+  },
+};
